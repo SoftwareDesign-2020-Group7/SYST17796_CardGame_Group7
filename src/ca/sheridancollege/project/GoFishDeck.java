1a914973d791718f8efa9bcb22192ccf1591f3a2
wucheny109 <wucheny@sheridancollege.ca>
2020-04-16 10:49:14 -0400

    Apr16 check

---
+++ src/ca/sheridancollege/project/GoFishDeck.java
@@ -0,0 +1,63 @@
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * @Modifier: Group7: Thanveer Hauzaree,Yuxiao Fang,Shuwen Wang,Chen-yu Wu
+ * @updateDate: 2020-04-12
+ */
+package ca.sheridancollege.project;
+
+import ca.sheridancollege.project.GoFishCard.Suit;
+import ca.sheridancollege.project.GoFishCard.Value;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GoFishDeck extends GroupOfCards {
+
+    private ArrayList<GoFishCard> cards = new ArrayList<>();
+    private int size;
+
+    public GoFishDeck(int givenSize) {
+        super(givenSize);
+        this.size = givenSize;
+    }
+
+    public ArrayList<GoFishCard> getCards() {
+        return this.cards;
+    }
+
+    @Override
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public void setCards(ArrayList<GoFishCard> cards) {
+        this.cards = cards;
+    }
+
+    public void initDeck() {
+        for (Suit s : Suit.values()) {
+            for (Value v : Value.values()) {
+                this.cards.add(new GoFishCard(s, v));
+            }
+        }
+    }
+
+    public void removeCard(GoFishCard card) {
+        cards.remove(card);
+    }
+
+    public void removeCards(ArrayList<GoFishCard> cards) {
+        for (GoFishCard card : cards) {
+            this.cards.remove(card);
+        }
+    }
+    
+    public GoFishCard DispatchOneCard(){
+        return this.cards.get(0);
+    }
+    
+    public void removeFirstCard(){
+        this.cards.remove(this.cards.get(0));
+    }
+    
+    
+}
---
+++ src/ca/sheridancollege/project/GoFishGame.java
@@ -0,0 +1,267 @@
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * @Modifier: Group7: Thanveer Hauzaree,Yuxiao Fang,Shuwen Wang,Chen-yu Wu
+ * @updateDate: 2020-04-12
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GoFishGame extends Game {
+
+    private GoFishDeck deck;
+    private ArrayList<GoFishPlayer> players = new ArrayList<>();
+    private ArrayList<GoFishHand> hands = new ArrayList<>();
+    private ArrayList<GoFishCard> cards = new ArrayList<>();
+    private static int numOfPlayers;
+
+    public GoFishGame(String givenName) {
+        // TODO - implement GoFishGame.GoFishGame
+        super(givenName);
+    }
+
+    public static void main(String[] args) {
+        GoFishGame gofish = new GoFishGame("GoFish");
+        System.out.println("welcome to " + gofish.getGameName());
+        System.out.println("please choose player numbers: 2,3,or 4(Only 2 players now)");
+        Scanner sc = new Scanner(System.in);
+        numOfPlayers = sc.nextInt();
+        gofish.startGame(numOfPlayers);
+
+        gofish.play(); // while there is card in the desk and hand, continue play
+        gofish.declareWinner();
+    }
+
+    /**
+     * When the game is over, use this method to declare and display a winning
+     * player.
+     */
+    @Override
+    public void declareWinner() {
+        // TODO - implement GoFishGame.declareWinner
+        System.out.println("players'score: ");
+        int max = 0;
+        String winner = "";
+        for (int i = 0; i < this.players.size(); i++) {
+            System.out.println(players.get(i).getPlayerID() + " : "
+                    + players.get(i).getHand().getScore());
+            if (players.get(i).getHand().getScore() > max) {
+                max = players.get(i).getHand().getScore();
+                winner = players.get(i).getPlayerID();
+            }
+        }
+        System.out.println("winner: " + winner + "  score: " + max);
+    }
+
+    /**
+     * Play the game. 
+     */
+    @Override
+    public void play() {
+
+        if (!deck.getCards().isEmpty()) {
+
+            Scanner sc = new Scanner(System.in);
+            String res = "";
+            for (int i = 0; i < numOfPlayers; i++) {
+                if (!hands.get(i).getCards().isEmpty()) {
+                    callTurns(i);
+                    System.out.println(players.get(i).getPlayerID() + "'s cards: " + hands.get(i).getCards());
+
+                    boolean isGofish = false;
+                    while (!isGofish) {
+                        isGofish = true;
+                        int numOfAskCard = 0;
+                        String valueOfAskCard = "";
+                        GoFishCard one = hands.get(0).getCards().get(0);
+
+                        if (i == numOfPlayers - 1) {
+                            System.out.println(players.get(0).getPlayerID() + "'s cards: " + hands.get(0).getCards());
+
+                            System.out.println(players.get(i).getPlayerID() + " asks " + players.get(0).getPlayerID()
+                                    + ": do you have any rank of ");
+                            String askCard = sc.nextLine();
+
+                            // next is index 0
+                            for (int sub = 0; sub < hands.get(0).getCards().size(); sub++) {
+                                GoFishCard card = hands.get(0).getCards().get(sub);
+                                String value = String.valueOf(card.getValue());
+                                if (value.equalsIgnoreCase(askCard)) {
+                                    hands.get(i).addCard(card);
+                                    hands.get(0).removeCard(card);
+                                    sub--;
+                                    numOfAskCard++;
+                                    one.setValue(card.getValue());
+                                    isGofish = false;
+                                }
+                            }
+                            if (!isGofish) {
+                                res = players.get(0).getPlayerID() + " says I have " + numOfAskCard + " " + one.getValue();
+                                System.out.println(res);
+
+                                if (hands.get(0).getCards().isEmpty()) {
+                                    hands.get(0).drawUpFiveCards(deck);
+                                }
+                            } else {
+                                res = players.get(0).getPlayerID() + " says: gofish";
+                                System.out.println(res);
+                                System.out.println("deck " + deck.getCards());
+                                if (!deck.getCards().isEmpty()) {
+                                    hands.get(i).drawOneCard(deck.DispatchOneCard());
+                                    deck.removeFirstCard();
+                                }
+                                i = -1; // next player will be players[0] when it reaches end of the list
+                            }
+
+                            checkBook(numOfPlayers - 1);
+
+                            if (endGame()) {
+                                break;
+                            }
+
+                        } else {
+//                            System.out.println(players.get(i + 1).getPlayerID() + "'s cards: "+hands.get(i + 1).getCards());
+
+                            System.out.println(players.get(i).getPlayerID() + " asks " + players.get(i + 1).getPlayerID()
+                                    + ": do you have any rank of ");
+                            String askCard = sc.nextLine();
+
+                            for (int sub = 0; sub < hands.get(i + 1).getCards().size(); sub++) {
+                                GoFishCard card = hands.get(i + 1).getCards().get(sub);
+                                String value = String.valueOf(card.getValue());
+
+                                if (value.equalsIgnoreCase(askCard)) {
+                                    hands.get(i).addCard(card);
+                                    hands.get(i + 1).removeCard(card);
+                                    sub--;
+                                    numOfAskCard++;
+                                    one.setValue(card.getValue());
+                                    isGofish = false;
+                                }
+                            }
+
+                            if (!isGofish) {
+                                res = players.get(i + 1).getPlayerID() + " says I have " + numOfAskCard + " " + one.getValue();
+                                System.out.println(res);
+                                if (hands.get(i + 1).getCards().isEmpty()) {
+                                    hands.get(i + 1).drawUpFiveCards(deck);
+                                }
+                            } else {
+                                res = players.get(i + 1).getPlayerID() + " says: gofish";
+                                System.out.println(res);
+                                System.out.println("deck " + deck.getCards());
+                                if (!deck.getCards().isEmpty()) {
+                                    hands.get(i).drawOneCard(deck.DispatchOneCard());
+                                    deck.removeFirstCard();
+                                }
+                            }
+
+                            checkBook(i);
+                            if (endGame()) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("No cards in the deck!");
+        }
+    }
+
+//    @Override
+//	public ArrayList<GoFishPlayer> getPlayers() {
+//		return this.players;
+//	}
+//	public void setPlayers(ArrayList<GoFishPlayer> players) {
+//		this.players = players;
+//	}
+    public void startGame(int numOfPlayers) {
+        this.deck = new GoFishDeck(52);
+        this.deck.initDeck();
+        this.deck.shuffle();
+        this.cards = deck.getCards();
+        System.out.println("deck's cards: " + cards);
+        switch (numOfPlayers) {
+            case 2: {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("please input first player name: ");
+                String nameOfPlayer1 = sc.nextLine();
+                System.out.println("please input second player name: ");
+                String nameOfPlayer2 = sc.nextLine();
+
+                GoFishPlayer player1 = new GoFishPlayer(nameOfPlayer1);
+                GoFishPlayer player2 = new GoFishPlayer(nameOfPlayer2);
+                GoFishHand hand1 = new GoFishHand();
+                GoFishHand hand2 = new GoFishHand();
+
+                ArrayList<GoFishCard> cardsOfPlayer1 = new ArrayList<>();
+                ArrayList<GoFishCard> cardsOfPlayer2 = new ArrayList<>();
+                for (int i = 0; i < 10; i += 2) {
+                    cardsOfPlayer1.add(cards.get(i));
+                    cardsOfPlayer2.add(cards.get(i + 1));
+                }
+                deck.removeCards(cardsOfPlayer1);
+                deck.removeCards(cardsOfPlayer2);
+//                System.out.println("deck " + deck.getCards());
+
+                hand1.setCards(cardsOfPlayer1);
+                hand2.setCards(cardsOfPlayer2);
+
+                hands.add(hand1);
+                hands.add(hand2);
+                player1.setHand(hand1);
+                player2.setHand(hand2);
+
+                players.add(player1);
+                players.add(player2);
+
+//                System.out.println(players.get(0).getPlayerID() + "'s cards: "
+//                        + hands.get(0).getCards());
+//                System.out.println(players.get(1).getPlayerID() + "'s cards: "
+//                        + hands.get(1).getCards());
+            }
+            break;
+            case 3:
+            case 4:
+                break;
+        }
+
+    }
+
+    public boolean endGame() {
+        // TODO - implement GoFishGame.endGame
+        boolean isEndGame = true;
+        for (int i = 0; i < this.players.size(); i++) {
+            if (!hands.get(i).getCards().isEmpty()) {
+                isEndGame = false;
+            }
+        }
+        if (deck.getCards().isEmpty() && isEndGame) {
+            System.out.println("Game over");
+        }
+        return isEndGame;
+    }
+
+    public void callTurns(int i) {
+        // TODO - implement GoFishGame.callTurns
+
+        System.out.println("It's " + players.get(i).getPlayerID() + "'s turn");
+
+    }
+
+    public void checkBook(int i) {
+        ArrayList<GoFishCard> book = hands.get(i).showBook();
+        if (!book.isEmpty()) {
+            System.out.println(players.get(i).getPlayerID() + " says I have a book : " + book);
+            hands.get(i).removeBook(book);
+
+            if (hands.get(i).getCards().isEmpty()) {
+                hands.get(i).drawUpFiveCards(deck);
+            }
+        }
+
+    }
+
+}
